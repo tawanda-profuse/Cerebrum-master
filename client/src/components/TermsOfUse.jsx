14712789472f5fe71fa2022d7bd3688469e506f8
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2024-06-03 03:26:59 +0200

    Created modals for privacy policy and terms of use

---
+++ client/src/components/TermsOfUse.jsx
@@ -0,0 +1,45 @@
+const TermsOfUse = ({ show, setShow }) => {
+    return (
+        <dialog
+            className={`w-[80vw] md:w-[50vw] overflow-y-scroll max-h-[80vh] absolute top-[10%] left-[50%] -translate-x-2/4 z-50 shadow-xl shadow-yedu-dark-gray py-4 px-8 rounded-lg ${show ? 'block' : 'hidden'}`}
+        >
+            <button className="absolute right-4 rounded-full bg-yedu-light-green py-1 px-3 text-2xl transition-all hover:scale-125">
+                <i
+                    className="fas fa-times"
+                    onClick={() => {
+                        setShow(false);
+                    }}
+                ></i>
+            </button>
+            <h1 className="text-3xl text-center font-bold underline my-12">
+                Terms of Use
+            </h1>
+            <p className="text-left text-yedu-gray-text my-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+                est! Perferendis quaerat sit possimus dignissimos nobis aliquid
+                explicabo rerum in pariatur dolorem? Consectetur, odit quidem
+                voluptatem maxime ab natus eaque culpa sint sed aut sit
+                molestias. Assumenda recusandae, odio asperiores facere soluta
+                doloribus nostrum quae omnis officia nobis minima quas!
+            </p>
+            <p className="text-left text-yedu-gray-text my-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+                est! Perferendis quaerat sit possimus dignissimos nobis aliquid
+                explicabo rerum in pariatur dolorem? Consectetur, odit quidem
+                voluptatem maxime ab natus eaque culpa sint sed aut sit
+                molestias. Assumenda recusandae, odio asperiores facere soluta
+                doloribus nostrum quae omnis officia nobis minima quas!
+            </p>
+            <p className="text-left text-yedu-gray-text my-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+                est! Perferendis quaerat sit possimus dignissimos nobis aliquid
+                explicabo rerum in pariatur dolorem? Consectetur, odit quidem
+                voluptatem maxime ab natus eaque culpa sint sed aut sit
+                molestias. Assumenda recusandae, odio asperiores facere soluta
+                doloribus nostrum quae omnis officia nobis minima quas!
+            </p>
+        </dialog>
+    );
+};
+
+export default TermsOfUse;
--- client/src/pages/GetStarted.jsx
+++ client/src/pages/GetStarted.jsx
@@ -1,56 +1,71 @@
+import { useState } from 'react';
 import logoGray from '../assets/logo-gray.svg';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import TermsOfUse from '../components/TermsOfUse';
+import Policy from '../components/Policy';
 
 const GetStarted = () => {
     const navigate = useNavigate();
+    const [termsOfUse, setTermsOfUse] = useState(false);
+    const [privacyPolicy, setPrivacyPolicy] = useState(false);
     return (
-        <section className="flex w-screen h-screen font-montserrat">
-            <div className="hidden md:block bg-yedu-light-gray w-2/4"></div>
-            <div className="w-full md:w-2/4 flex flex-col justify-center items-center relative">
-                <h1 className="text-yedu-dark text-3xl">Get Started</h1>
-                <div className="flex justify-center gap-4 mt-3 flex-wrap">
-                    <button
-                        className="bg-yedu-green py-2 px-4 text-white rounded-md border-none outline-none text-yedu-white w-32 hover:opacity-80"
-                        onClick={() => navigate('/user/login')}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className="bg-yedu-green py-2 px-4 text-white rounded-md border-none outline-none text-yedu-white w-32 hover:opacity-80"
-                        onClick={() => navigate('/user/signup')}
-                    >
-                        Sign up
-                    </button>
-                </div>
-                <div className="absolute bottom-4">
-                    <div className="flex justify-center items-center gap-2 mb-4">
-                        <img
-                            src={logoGray}
-                            alt=""
-                            className="w-8 fill-yedu-light-gray"
-                        />
-                        <h3 className="text-yedu-dark-gray font-medium">
-                            YeduAI
-                        </h3>
-                    </div>
-                    <div className="flex gap-3 justify-center">
-                        <Link
-                            to=""
-                            className="text-yedu-dark-gray underline hover:no-underline"
+        <>
+            <TermsOfUse show={termsOfUse} setShow={setTermsOfUse} />
+            <Policy display={privacyPolicy} setDisplay={setPrivacyPolicy} />
+            <section className="flex w-screen h-screen font-montserrat">
+                <div className="hidden md:block bg-yedu-light-gray w-2/4"></div>
+                <div className="w-full md:w-2/4 flex flex-col justify-center items-center relative">
+                    <h1 className="text-yedu-dark text-3xl">Get Started</h1>
+                    <div className="flex justify-center gap-4 mt-3 flex-wrap">
+                        <button
+                            className="bg-yedu-green py-2 px-4 text-white rounded-md border-none outline-none text-yedu-white w-32 hover:opacity-80"
+                            onClick={() => navigate('/user/login')}
                         >
-                            Terms of Use
-                        </Link>
-                        <p className="text-yedu-dark-gray">|</p>
-                        <Link
-                            to=""
-                            className="text-yedu-dark-gray underline hover:no-underline"
+                            Login
+                        </button>
+                        <button
+                            className="bg-yedu-green py-2 px-4 text-white rounded-md border-none outline-none text-yedu-white w-32 hover:opacity-80"
+                            onClick={() => navigate('/user/signup')}
                         >
-                            Privacy Policy
-                        </Link>
+                            Sign up
+                        </button>
+                    </div>
+                    <div className="absolute bottom-4">
+                        <div className="flex justify-center items-center gap-2 mb-4">
+                            <img
+                                src={logoGray}
+                                alt=""
+                                className="w-8 fill-yedu-light-gray"
+                            />
+                            <h3 className="text-yedu-dark-gray font-medium">
+                                YeduAI
+                            </h3>
+                        </div>
+                        <div className="flex gap-3 justify-center">
+                            <button
+                                className="text-yedu-dark-gray underline hover:no-underline"
+                                onClick={() => {
+                                    setTermsOfUse(true);
+                                    setPrivacyPolicy(false);
+                                }}
+                            >
+                                Terms of Use
+                            </button>
+                            <p className="text-yedu-dark-gray">|</p>
+                            <button
+                                className="text-yedu-dark-gray underline hover:no-underline"
+                                onClick={() => {
+                                    setPrivacyPolicy(true);
+                                    setTermsOfUse(false);
+                                }}
+                            >
+                                Privacy Policy
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
