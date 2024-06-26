9513da9ebcdf072a5ecb59b1e02a2eec8247ee46
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2024-06-26 11:47:44 +0200

    Adjusted width of login modal in mobile view

--- client/src/components/Modals/Login.jsx
+++ client/src/components/Modals/Login.jsx
@@ -76,7 +76,7 @@ const Login = ({
                 className={`fixed modal-content inset-0 z-10 overflow-y-auto ${display ? 'block' : 'hidden'}`}
             >
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-4/5 md:w-full md:max-w-lg">
                         <div className="bg-gradient-to-br from-gray-50 to-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
