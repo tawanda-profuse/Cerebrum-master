76775a5a0c1168e25b17a5fd7130afb206b3c306
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2024-07-11 17:41:56 +0200

    Update dark mode contrast in file upload modal

--- client/src/components/Modals/FileManagement/FileUpload.jsx
+++ client/src/components/Modals/FileManagement/FileUpload.jsx
@@ -65,12 +65,12 @@ const FileUpload = ({ display, setDisplay }) => {
                 className={`fixed inset-0 bg-black bg-opacity-50 z-50 ${display ? 'block' : 'hidden'}`}
             ></div>
             <dialog
-                className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-xl p-6 w-11/12 max-w-md z-50"
+                className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark-applied rounded-lg shadow-xl p-6 w-11/12 max-w-md z-50"
                 open={display}
                 ref={fileUploadRef}
             >
                 <button
-                    className="absolute right-2 top-2 text-gray-500 hover:text-gray-700 bg-green-100 rounded-full w-8 h-8 flex items-center justify-center"
+                    className="absolute right-2 top-2 text-gray-500 dark:text-white hover:text-gray-700 bg-green-100 dark:bg-green-500 rounded-full w-8 h-8 flex items-center justify-center"
                     onClick={() => setDisplay(false)}
                 >
                     <i className="fas fa-times"></i>
@@ -80,7 +80,7 @@ const FileUpload = ({ display, setDisplay }) => {
                 </h1>
                 <div className="grid grid-cols-2 gap-4">
                     <button
-                        className="bg-gray-200 hover:bg-gray-300 rounded-lg p-4 flex flex-col items-center justify-center transition-colors"
+                        className="bg-gray-200 dark:bg-[#333] hover:bg-gray-300 dark:hover:bg-[#666] rounded-lg p-4 flex flex-col items-center justify-center transition-colors"
                         onClick={() => {
                             setSketchUpload(true);
                             setDisplay(false);
@@ -90,7 +90,7 @@ const FileUpload = ({ display, setDisplay }) => {
                         <span>Upload a Sketch</span>
                     </button>
                     <button
-                        className="bg-gray-200 hover:bg-gray-300 rounded-lg p-4 flex flex-col items-center justify-center transition-colors"
+                        className="bg-gray-200 dark:bg-[#333] hover:bg-gray-300 dark:hover:bg-[#666] rounded-lg p-4 flex flex-col items-center justify-center transition-colors"
                         onClick={() => {
                             socket.emit(
                                 'get-project-completed',
